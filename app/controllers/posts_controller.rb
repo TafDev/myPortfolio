48fb9eda56b8fa4c3be01fea1c7227da1c79fecb
tafdev <chombot@hotmail.com>
2016-07-06 20:13:01 +0000

    setup project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :update, :edit, :destroy]
   
   def index
-    
+    @posts = Post.all.order("created_at desc")
   end
   
   def new
